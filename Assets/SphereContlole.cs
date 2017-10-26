490402e919c1f3bbce3486c0f14b961bf23150cb
Eiji Nagata <nkc.eiji.nagata>
2017-10-26 10:25:59 +0900

    玉が置かれた際にフラグをtrueにして、その時にマウスのボタンを押すと消去する処理を追加

--- Assets/SphereContlole.cs
+++ Assets/SphereContlole.cs
@@ -3,18 +3,20 @@ using System.Collections;
 
 public class SphereContlole : MonoBehaviour {
 
+    bool DliatFlag = false;
 	// Use this for initialization
     public void shoot(Vector3 dir)
     {
         GetComponent<Rigidbody>().AddForce(dir);
     }
-    private void OnCollisionEnter(Collision other)
+    private void OnCollisionEnter(Collision other)//アタッチされたゲームオブジェクトが何かにぶつかった際の判定。
     {
-        if (other.gameObject.tag == "Ground")
+        if (other.gameObject.tag == "Ground")//もし衝突した対象がGroundのタグを持つオブジェクトだった場合。
         {
             GetComponent<Rigidbody>().isKinematic = true;
+            DliatFlag = true;
             Debug.Log("判定おけ");
-            Destroy(this.gameObject);
+            //Destroy(this.gameObject);
         }
     }
     void Start () {
@@ -24,6 +26,15 @@ public class SphereContlole : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(DliatFlag==true)
+            {
+                Destroy(this.gameObject);
+                DliatFlag = false;
+            }
+        }
+
 	}
 }
