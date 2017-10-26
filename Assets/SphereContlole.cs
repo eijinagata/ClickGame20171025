30914d5c7b887681f4bc24509ac7ee5742d2a599
Eiji Nagata <nkc.eiji.nagata>
2017-10-26 09:53:31 +0900

    タグと衝突判定を追加（タグ取得はまだ）

---
+++ Assets/SphereContlole.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereContlole : MonoBehaviour {
+
+	// Use this for initialization
+    public void shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
